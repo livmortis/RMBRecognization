00ccf4a421d65c950b7eca7abaff371451cca7b6
xzy <xzy@192-200-240-217.mci.googlefiber.net>
2019-05-28 12:37:06 +0800

    add code of calculate iou for train, and draw box in image for test

--- formal/detection/fdConfig.py
+++ formal/detection/fdConfig.py
@@ -9,12 +9,18 @@ IMG_SIZE_WIDTH = 224
 IMG_SIZE_HEIGHT = 224
 BATCH_SIZE = 32
 WHICH_MODEL = 'R'   #'R'是回归模型，'E'是EAST模型。
-LR = 0.01
 WEIGHT_DECAY =  0.0001
 lr_exponential_gamma = 0.9
-LOG_FOR_NET_CONSTRUCTION = False
+train_cal_iou_num = 15
+LOG_FOR_NET_CONSTRUCTION = True
+
+lr_patient = 7
+lr_shrink_factor = 0.5
+LR = 0.0001
 
 use_gpu = False
 is_test = True
-need_load_model = False
+test_test_num = 80
+test_train_num = 30
+need_load_model = True
 
--- formal/detection/fdTest.py
+++ formal/detection/fdTest.py
@@ -9,14 +9,26 @@ from tqdm import tqdm
 import torch
 import cv2
 
-def drawRect(pred_batch, y_batch):
+
+def drawRect(pred_batch, y_batch, type):
   i = 0
   for pred in pred_batch:
     y = y_batch[i]
     img_path = fdConfig.train_img_path +str(y)
     img = cv2.imread(img_path)
-    print("minx is : "+str(pred[0]))
-    rect = cv2.rectangle(img, (pred[0],pred[1]),(pred[2],pred[3]),color=(0,255,255),thickness=1)
+    img = cv2.resize(img,(fdConfig.IMG_SIZE_HEIGHT * 2,fdConfig.IMG_SIZE_HEIGHT))
+    # print("minx is : "+str(pred[0]))
+    if type=='Reg':
+      adjust_xmin = int(pred[0]-10) #目测box位置，手动调整 (regression模型loss=1.5，结果一般)
+      adjust_ymin = int(pred[1]-5)  #目测box位置，手动调整
+      adjust_xmax = int(pred[2]+10)  #目测box位置，手动调整
+      adjust_ymax = int(pred[3]+10) #目测box位置，手动调整
+    else:
+      adjust_xmin = pred[0]
+      adjust_ymin = pred[1]
+      adjust_xmax = pred[2]
+      adjust_ymax = pred[3]
+    rect = cv2.rectangle(img, (adjust_xmin,adjust_ymin),(adjust_xmax,adjust_ymax),color=(0,255,255),thickness=1)
     cv2.imshow("a",rect)
     cv2.waitKey(0)
 
@@ -26,6 +38,7 @@ def drawRect(pred_batch, y_batch):
 if __name__ == "__main__":
   if fdConfig.WHICH_MODEL == 'R':
     model_R = torch.load(fdConfig.model_saved + "detect_reg_model.pkl")
+    # model_R = torch.load(fdConfig.model_saved + "detect_reg_model_cpu_loss15_epo8.pkl")
     dataset_R = fdData_Regression.FdTestDataReg()
     testDataloader_R = Dataloader.DataLoader(dataset_R, fdConfig.BATCH_SIZE, shuffle=False)
 
@@ -39,7 +52,8 @@ if __name__ == "__main__":
         pred_np = prediction.detach().cpu().numpy()
       else:
         pred_np = prediction.detach().numpy()
-      print(pred_np)
-      print(y)
-      drawRect(pred_np, y)
+      # print(pred_np)
+      # print(y)
+      drawRect(pred_np, y, 'Reg')
+
       prediction_list.append(pred_np)
