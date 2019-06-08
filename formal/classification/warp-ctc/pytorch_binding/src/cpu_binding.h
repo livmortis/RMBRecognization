1d3c6e7f35e6f44b3f4c1240d5e3cdbbeb346b9f
xzy <xzy@xzyPro.local>
2019-06-08 17:26:11 +0800

    add crnn dependency

---
+++ formal/classification/warp-ctc/pytorch_binding/src/cpu_binding.h
@@ -0,0 +1,8 @@
+int cpu_ctc(THFloatTensor *probs,
+                        THFloatTensor *grads,
+                        THIntTensor *labels_ptr,
+                        THIntTensor *label_sizes_ptr,
+                        THIntTensor *sizes,
+                        int minibatch_size,
+                        THFloatTensor *costs,
+                        int blank_label);
