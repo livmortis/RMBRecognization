eea1d0d4f586c6d67cfdaa2abbe5baa74e5a65d1
xzy <xzy@xzyPro.local>
2019-06-11 00:02:51 +0800

    test code

--- formal/classification/crnnClassify/test_xzy.py
+++ formal/classification/crnnClassify/test_xzy.py
@@ -6,6 +6,7 @@ import utils
 import models.crnn as crnn
 from torch.autograd import Variable
 import pandas as pd
+import numpy as np
 
 saved_model_path = "expr/netCRNN_24_500.pth"
 
@@ -18,18 +19,20 @@ use_cuda = True
 
 
 
-def test_by_xzy(net,test_dataset):
+def test_by_xzy(net, test_dataset):
   print('Start test')
 
+
+  for p in net.parameters():
+      p.requires_grad = False
 
   net.eval()
   data_loader = torch.utils.data.DataLoader(
     test_dataset, shuffle=True, batch_size=64, num_workers=int(2))
   val_iter = iter(data_loader)
 
-  i = 0
-  n_correct = 0
-  # loss_avg = utils.averager()
+  img_name_List = []
+  img_pred_List = []
 
   for i in range(len(data_loader)):
     data = val_iter.next()
@@ -51,15 +54,23 @@ def test_by_xzy(net,test_dataset):
     preds = preds.transpose(1, 0).contiguous().view(-1)
     sim_preds = converter.decode(preds.data, preds_size.data, raw=False)    #sim_preds是预测出的字符串,类似“ XG78233838 ”
 
-    img_name_List = []
-    img_pred_List = []
+
     for pred, name in zip(sim_preds, cpu_img_name):
-      img_name_List.append(str(name))
-      img_pred_List.append(str(pred))
-    pd.DataFrame({''})
+      img_name_List.extend(str(name))
+      img_pred_List.extend(str(pred))
 
-  raw_preds = converter.decode(preds.data, preds_size.data, raw=True)[:666]   #raw_preds是长的那种....，类似 “XG--------7-8-2-3--3-8-3-8”
+  img_pred_List = np.array(img_pred_List)
+  img_name_List = np.array(img_name_List)
 
+  df = pd.DataFrame({'name':img_name_List, 'label':img_pred_List})
+  column_order = ['name','label']
+  df = df[column_order]
+  predictionFile = '../../../../dataset_formal/classify_data/crnnData/result/crnn_result.csv'
+  df.to_csv(predictionFile, index=False)
+
+  print("\nover")
+
+  # raw_preds = converter.decode(preds.data, preds_size.data, raw=True)[:666]   #raw_preds是长的那种....，类似 “XG--------7-8-2-3--3-8-3-8”
 
 
 
@@ -71,7 +82,7 @@ def test_by_xzy(net,test_dataset):
 
 
 if __name__ == "__main__":
-  test_lmdb_path = "../../../../dataset_formal/classify_data/crnnData/trainDataLMDB"
+  test_lmdb_path = "../../../../dataset_formal/classify_data/crnnData/testDataLMDB"
 
   test_dataset = dataset.lmdbDataset(root=test_lmdb_path, type="test")
 
@@ -92,22 +103,11 @@ if __name__ == "__main__":
 
   print('loading pretrained model from %s' % saved_model_path)
   crnn.load_state_dict(torch.load(saved_model_path))
-
   print(crnn)
 
-  image = Variable(image)
-  text = Variable(text)
-  length = Variable(length)
-
-  # loss averager
-  loss_avg = utils.averager()
-
-
-  test_iter = iter(test_loader)
-
-
-  for p in crnn.parameters():
-      p.requires_grad = False
+  # image = Variable(image)
+  # text = Variable(text)
+  # length = Variable(length)
 
 
   test_by_xzy(crnn,test_dataset)
