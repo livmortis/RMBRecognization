a4e4000a11a5daee1416ba065f354c5392073b81
xzy <xzy@192-200-241-27.mci.googlefiber.net>
2019-06-12 19:05:11 +0800

    rebuild project

---
+++ formal/detection2_CTPN/main/ctpnExperiment.py
@@ -0,0 +1,30 @@
+
+import cv2
+import os
+
+# # 寻找正确数据路径
+# path = "../../../../dataset_formal/detect_data/CTPNData/image"
+# pic = "../../../../../dataset_formal/detect_data/CTPNData/image/0A2PDULI.jpg"
+# list = os.listdir(path)
+# print(list[0])
+#
+# a = cv2.imread(path)
+# print(a)
+# # cv2.imshow("a",a)
+# # cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
