13eb2e47fad1d94c0de8c4b6ea85f54b892f6180
xzy <xzy@xzyPro.local>
2019-06-24 08:55:52 +0800

    training 0.5 yuan

--- formal/detection2_CTPN/utils/dataset/data_provider.py
+++ formal/detection2_CTPN/utils/dataset/data_provider.py
@@ -14,11 +14,11 @@ from utils.dataset.data_util import GeneratorEnqueuer
 # DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/0_1_ctpn_img"                 #xzy 1800标注图片 + 分面值训练
 # DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/0_1_ctpn_label/"            #xzy 1800标注图片 + 分面值训练
 
-DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/0_2_ctpn_img"
-DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/0_2_ctpn_label/"
+# DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/0_2_ctpn_img"
+# DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/0_2_ctpn_label/"
 #
-# DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/0_5_ctpn_img"
-# DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/0_5_ctpn_label/"
+DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/0_5_ctpn_img"
+DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/0_5_ctpn_label/"
 #
 # DATA_FOLDER_IMG = "../../../dataset_formal/detect_data/CTPNData/1_ctpn_img"
 # DATA_FOLDER_LABEL = "../../../dataset_formal/detect_data/CTPNData/1_ctpn_label/"
